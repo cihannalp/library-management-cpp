8b54207d6b96426bac4418f1c9bb7fe2532590f9
Cihan Alp <cihannalp@gmail.com>
2018-12-14 11:44:19 +0300

    Initial commit

---
+++ src/LibraryManagement.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : LibraryManagement.cpp
+// Author      : Cihan Alp
+// Version     :
+// Copyright   : 
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Hi" << endl; // prints Hi
+	return 0;
+}
